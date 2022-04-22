d3a3a0eb7eece4a0d2c8bc0ee820763de874ce62
Gabriel Mendes <gabriel.mendes@ciss.com.br>
2022-04-22 12:57:06 -0300

    "Instantiating categories"

--- src/main/java/com/gabrielmendes/cursomc/CursomcApplication.java
+++ src/main/java/com/gabrielmendes/cursomc/CursomcApplication.java
@@ -1,13 +1,31 @@
 package com.gabrielmendes.cursomc;
 
+import com.gabrielmendes.cursomc.domain.Category;
+import com.gabrielmendes.cursomc.repositories.CategoryRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoryRespository categoryRespository;
 
     public static void main(String[] args){
         SpringApplication.run(CursomcApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Category cat1 = new Category(null, "Informatic");
+        Category cat2 = new Category(null, "Office");
+        List<Category> categories = Arrays.asList(cat1, cat2);
+
+        categoryRespository.saveAll(categories);
+    }
 }
--- src/main/java/com/gabrielmendes/cursomc/domain/Category.java
+++ src/main/java/com/gabrielmendes/cursomc/domain/Category.java
@@ -22,7 +22,7 @@ public class Category implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     private String name;
 
 }
