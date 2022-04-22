51c5948dd16d69bf093200e839736511e4012a74
Gabriel Mendes <gabriel.mendes@ciss.com.br>
2022-04-22 12:33:58 -0300

    h2database

--- src/main/java/com/gabrielmendes/cursomc/domain/Category.java
+++ src/main/java/com/gabrielmendes/cursomc/domain/Category.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 
@@ -19,6 +21,7 @@ import java.io.Serializable;
 public class Category implements Serializable {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
 
